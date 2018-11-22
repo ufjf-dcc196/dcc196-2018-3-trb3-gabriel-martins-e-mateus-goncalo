507abf985c30a99b1643deaae7c0c79b37845293
Gabriel <gabrielmartinssantana@hotmail.com>
2018-11-22 09:26:22 -0200

    Inicia o projeto e cria ListaActivity

---
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ufjf.dcc196.dcc196_trb03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
