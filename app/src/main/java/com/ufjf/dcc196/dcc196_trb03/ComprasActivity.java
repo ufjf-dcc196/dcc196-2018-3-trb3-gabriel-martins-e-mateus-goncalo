82d1499f0fac62e300761f1df4f45061e38246c7
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-11-22 10:27:37 -0200

    Cria o Compras Activity

---
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ComprasActivity.java
@@ -0,0 +1,18 @@
+package com.ufjf.dcc196.dcc196_trb03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class ComprasActivity extends AppCompatActivity {
+
+    private RecyclerView rvCompras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compras);
+
+        rvCompras = (RecyclerView) findViewById(R.id.rv_compras);
+    }
+}
