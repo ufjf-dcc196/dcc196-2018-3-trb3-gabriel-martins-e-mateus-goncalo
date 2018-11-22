a585ce4484ec6f1509a329bfb14f15cd3b819d3b
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-11-22 09:58:52 -0200

    Trata comunicação inicial entre o ListaActivity e o CadastrarListaActivity

--- app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ListaActivity.java
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ListaActivity.java
@@ -1,7 +1,9 @@
 package com.ufjf.dcc196.dcc196_trb03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ListaActivity extends AppCompatActivity {
@@ -16,5 +18,13 @@ public class ListaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista);
 
         btnNovaLista = (Button) findViewById(R.id.btn_NovaLista);
+
+        btnNovaLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaActivity.this, CadastrarListaActivity.class);
+                startActivityForResult(intent, ListaActivity.REQUEST_NOVALISTA);
+            }
+        });
     }
 }
