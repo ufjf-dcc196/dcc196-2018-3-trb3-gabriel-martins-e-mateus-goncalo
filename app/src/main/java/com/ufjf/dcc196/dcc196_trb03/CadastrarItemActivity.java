0471ddfa051e19c13b9cb08ca8fc3011966975f3
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-11-22 10:22:24 -0200

    Trata comunicação inicial entre o ItensListaActivity e o CadastrarItemActivity

--- app/src/main/java/com/ufjf/dcc196/dcc196_trb03/CadastrarItemActivity.java
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/CadastrarItemActivity.java
@@ -1,7 +1,9 @@
 package com.ufjf.dcc196.dcc196_trb03;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +21,13 @@ public class CadastrarItemActivity extends AppCompatActivity {
         btnConfNovoItem = (Button) findViewById(R.id.btn_ConfNovoItem);
         etxtNomeItem = (EditText) findViewById(R.id.etxt_NomeItem);
         etxtQuantidadeItem = (EditText) findViewById(R.id.etxt_QtdItem);
+
+        btnConfNovoItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ItensListaActivity.java
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ItensListaActivity.java
@@ -1,11 +1,15 @@
 package com.ufjf.dcc196.dcc196_trb03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 public class ItensListaActivity extends AppCompatActivity {
+
+    private static final int REQUEST_NOVOITEM = 1;
 
     private Button btnAddItem;
     private RecyclerView rvItensLista;
@@ -17,5 +21,13 @@ public class ItensListaActivity extends AppCompatActivity {
 
         btnAddItem = (Button) findViewById(R.id.btn_addItem);
         rvItensLista = (RecyclerView) findViewById(R.id.rv_itensLista);
+
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItensListaActivity.this, CadastrarItemActivity.class);
+                startActivityForResult(intent, ItensListaActivity.REQUEST_NOVOITEM);
+            }
+        });
     }
 }
