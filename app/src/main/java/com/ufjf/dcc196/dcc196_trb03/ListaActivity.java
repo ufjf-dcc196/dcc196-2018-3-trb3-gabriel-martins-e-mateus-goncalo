d2e346193c9ba57d163322e1fda9023c74e02519
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-11-22 09:42:12 -0200

    Trata comunicação inicial entre o MainActivity e o ListaActivity

--- app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ListaActivity.java
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ListaActivity.java
@@ -2,12 +2,19 @@ package com.ufjf.dcc196.dcc196_trb03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ListaActivity extends AppCompatActivity {
+
+    private static final int REQUEST_NOVALISTA = 1;
+
+    private Button btnNovaLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        btnNovaLista = (Button) findViewById(R.id.btn_NovaLista);
     }
 }
--- app/src/main/java/com/ufjf/dcc196/dcc196_trb03/MainActivity.java
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/MainActivity.java
@@ -1,13 +1,33 @@
 package com.ufjf.dcc196.dcc196_trb03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LISTA = 1;
+    private static final int REQUEST_COMPRA = 2;
+
+    private Button btnListas;
+    private Button btnGerenCompras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnListas = (Button) findViewById(R.id.btn_Listas);
+        btnGerenCompras = (Button) findViewById(R.id.btn_GerencCompras);
+
+        btnListas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListaActivity.class);
+                startActivityForResult(intent, MainActivity.REQUEST_LISTA);
+            }
+        });
     }
 }
