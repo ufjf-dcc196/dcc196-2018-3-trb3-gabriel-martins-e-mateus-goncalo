36dc78326021d527151cac528ef67c202f45eaae
Mateus Gonçalo <mateusgon57@gmail.com>
2018-11-29 11:49:37 -0200

    Adicionando o ListaAdapter ao sistema e o Lista_Layout

---
+++ app/src/main/java/com/ufjf/dcc196/dcc196_trb03/ListaAdapter.java
@@ -0,0 +1,127 @@
+package com.ufjf.dcc196.dcc196_trb03;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ListaAdapter extends RecyclerView.Adapter<ListaAdapter.ViewHolder>{
+
+    private Cursor cursor;
+    private OnListaClickListener listener;
+    private OnListaLongClickListener longListener;
+
+    public interface OnListaClickListener {
+        void onListaClick(View listaView, int position);
+    }
+    public interface OnListaLongClickListener {
+        void onListaLongClick(View listaView, int position);
+    }
+
+    public void setOnListaClickListener(OnListaClickListener listener){
+        this.listener = listener;
+    }
+    public void setOnListaLongClickListener(OnListaLongClickListener listener){
+        this.longListener = listener;
+    }
+
+    public ListaAdapter (Cursor c)
+    {
+        cursor = c;
+    }
+
+    public void setCursor(Cursor cursor) {
+        this.cursor = cursor;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View eventoView = inflater.inflate(R.layout.listas_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(eventoView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        int idxNum = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_REGISTRO);
+        int idxNome = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_NOME);
+
+        cursor.moveToPosition(i);
+
+        viewHolder.numEvento.setText(String.valueOf(cursor.getInt(idxNum)));
+        viewHolder.nomeEvento.setText(cursor.getString(idxNome));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        public TextView txtNomeEvento;
+        public TextView txtNumeroEvento;
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            txtNumeroEvento = itemView.findViewById(R.id.txtNumLista);
+            txtNomeEvento = itemView.findViewById(R.id.txtListaNome);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener!=null){
+                        int position = getAdapterPosition();
+                        if(position!=RecyclerView.NO_POSITION){
+                            listener.onListaClick(itemView, position);
+                        }
+                    }
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if(longListener!=null){
+                        int position = getAdapterPosition();
+                        if(position!=RecyclerView.NO_POSITION){
+                            longListener.onListaLongClick(itemView, Integer.parseInt(txtNumeroEvento.getText().toString()));
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(listener!=null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    listener.onListaClick(v, position);
+                }
+            }
+        }
+        @Override
+        public boolean onLongClick(View v) {
+            if(longListener!=null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    longListener.onListaLongClick(v, position);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
